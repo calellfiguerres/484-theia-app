f0fc06882bdcf793fc11656bd816539af07c1171
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-12-03 21:12:19 -0800

    Added debounce and moving direction

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from '@remix-run/react';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const DeviceAcceleration = () => {
   const [acceleration, setAcceleration] = useState({
@@ -8,18 +8,35 @@ const DeviceAcceleration = () => {
     z: null,
   });
   const navigate = useNavigate();
+  const lastUpdateTime = useRef(Date.now());
+
+  const [movingX, setMovingX] = useState(false);
+  const [movingY, setMovingY] = useState(false);
+  const [movingZ, setMovingZ] = useState(false);
+
 
   
   const [fallDetected, setFallDetected] = useState(false);
 
   useEffect(() => {
     const handleMotion = (event) => {
+        const currentTime = Date.now();
+        if (currentTime - lastUpdateTime.current < 200) {
+            return;
+        }
+        lastUpdateTime.current = currentTime;
+
+
       if (event.acceleration) {
         setAcceleration({
           x: event.acceleration.x || 0,
           y: event.acceleration.y || 0,
           z: event.acceleration.z || 0,
         });
+
+        setMovingX(Math.abs(event.acceleration.x) > 0.30);
+        setMovingY(Math.abs(event.acceleration.y) > 0.30);
+        setMovingZ(Math.abs(event.acceleration.z) > 0.30);
 
 
         const magnitude = Math.sqrt(
@@ -53,9 +70,9 @@ const DeviceAcceleration = () => {
     <div style={{ fontFamily: 'Arial, sans-serif', padding: '1rem' }}>
       <h2>Device Acceleration</h2>
       <h2>Fall Detected: {fallDetected ? "true" : "false"}</h2>
-      <p><strong>X:</strong> {acceleration.x !== null ? acceleration.x.toFixed(2) : 'N/A'}</p>
-      <p><strong>Y:</strong> {acceleration.y !== null ? acceleration.y.toFixed(2) : 'N/A'}</p>
-      <p><strong>Z:</strong> {acceleration.z !== null ? acceleration.z.toFixed(2) : 'N/A'}</p>
+      <p>{movingX ? "Moving" : null}<strong>X:</strong> {acceleration.x !== null ? acceleration.x.toFixed(2) : 'N/A'}</p>
+      <p>{movingY ? "Moving" : null}<strong>Y:</strong> {acceleration.y !== null ? acceleration.y.toFixed(2) : 'N/A'}</p>
+      <p>{movingZ ? "Moving" : null}<strong>Z:</strong> {acceleration.z !== null ? acceleration.z.toFixed(2) : 'N/A'}</p>
     </div>
   );
 };
